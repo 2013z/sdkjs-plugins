0f80992746e5e901d707c6103c02d3b48a23d414
eysp <60641231+eysp@users.noreply.github.com>
2021-11-06 19:34:07 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+red='\033[0;31m'
+plain='\033[0m'
+
+#默认安装目录/var/www/onlyoffice/documentserver
+name=/var/www/onlyoffice/documentserver
+
+clear
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
+
+cd /opt
+
+
+curl -sL https://github.com/eysp/sdkjs-plugins/archive/refs/tags/sdkjs-plugins.tar.gz | tar xz
+
+cp -rf sdkjs-plugins-sdkjs-plugins/* $name/sdkjs-plugins/*
+
