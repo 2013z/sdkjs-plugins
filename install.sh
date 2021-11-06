eb1ab66e14375539e2e29af2d35c5a2b185ed48f
eysp <60641231+eysp@users.noreply.github.com>
2021-11-06 19:06:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ name=/var/www/onlyoffice/documentserver
 down=/opt
 
 [[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
-echo -e "${plain}"
+
 cd $down
 
 curl -sL https://github.com/eysp/sdkjs-plugins/archive/refs/tags/sdkjs-plugins.tar.gz | tar xz       
 mv sdkjs-plugins-sdkjs-plugins sdkjs-plugins
-echo -e "安装目录：${red} \n（docker版直接回车，默认目录$name）\n"
+echo -e "安装目录：（docker版直接回车，默认目录$name）\n"
 #read -p "直接回车，apt等方式安装的自己查找目录输入${plain}" webdir
 read -p "输入目录名 docker版留空默认即可：$name" webdir
 
