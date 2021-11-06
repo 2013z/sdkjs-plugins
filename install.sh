dd934bf6a96216c4bb1af11b6c4b2500d26ddf7e
eysp <60641231+eysp@users.noreply.github.com>
2021-11-06 18:19:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,25 +6,25 @@ plain='\033[0m'
 dir=/var/www/onlyoffice/documentserver
 [[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
-#cd /opt
+cd /opt
 
 curl -sL https://github.com/eysp/sdkjs-plugins/archive/refs/tags/sdkjs-plugins.tar.gz | tar xz
     if [[ ! -n "sdkjs-plugins-sdkjs-plugins" ]]; then
     echo "下载汉化文件失败，请检查网络"
     fi
 #    exit 1
-mv sdkjs-plugins-sdkjs-plugins sdkjs-plugins
-echo -e "输入汉化文件安装目录：${red} \n（docker版直接回车，默认目录$dir）\n"
-read -p "直接回车，apt等方式安装的自己查找目录输入" webdir
+#mv sdkjs-plugins-sdkjs-plugins sdkjs-plugins
+#echo -e "输入汉化文件安装目录：${red} \n（docker版直接回车，默认目录$dir）\n"
+#read -p "直接回车，apt等方式安装的自己查找目录输入" webdir
 #echo -e "${red}"
-    if [[ ! -n "$webdir" ]]; then
-        webdir=$dir
-    fi
-cp -rf  sdkjs-plugins $webdir
+#    if [[ ! -n "$webdir" ]]; then
+#        webdir=$dir
+#    fi
+#cp -rf  sdkjs-plugins $webdir
 
 #echo -e "${plain}"
-    if [[ ! -n "$webdir/sdkjs-plugins/README.md" ]]; then
-        echo "${red}汉化成功，清空浏览器缓存即可看到onlyoffice插件是中文到了"
-    else
-        echo "痛哭，汉化失败了${plain}"
-    fi
+#    if [[ ! -n "$webdir/sdkjs-plugins/README.md" ]]; then
+#        echo "${red}汉化成功，清空浏览器缓存即可看到onlyoffice插件是中文到了"
+#    else
+#        echo "痛哭，汉化失败了${plain}"
+#    fi
