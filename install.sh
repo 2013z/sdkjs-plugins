ab1efb282a7ad4877f11134d58262bdeabd7d3f5
eysp <60641231+eysp@users.noreply.github.com>
2021-11-06 18:03:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ plain='\033[0m'
 
 #默认安装目录
 dir=/var/www/onlyoffice/documentserver
+[[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
 cd /opt
 
@@ -11,7 +12,7 @@ curl -sSL https://github.com/eysp/sdkjs-plugins/archive/refs/tags/sdkjs-plugins.
     if [[ ! -n "sdkjs-plugins-sdkjs-plugins" ]]; then
     echo "下载汉化文件失败，请检查网络"
     fi
-    exit 1
+#    exit 1
 mv sdkjs-plugins-sdkjs-plugins sdkjs-plugins
 echo -e "输入汉化文件安装目录：${red} \n（docker版直接回车，默认目录$dir）\n"
 read -p "直接回车，apt等方式安装的自己查找目录输入" webdir
