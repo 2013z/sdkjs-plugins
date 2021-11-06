0b247349f8fa89a06741e139ed8c1fcfb34f65b3
eysp <60641231+eysp@users.noreply.github.com>
2021-11-06 17:45:29 +0800

    一键安装脚本

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+red='\033[0;31m'
+plain='\033[0m'
+
+#默认安装目录
+dir=/var/www/onlyoffice/documentserver
+
+cd /opt
+
+curl -sSL https://github.com/eysp/sdkjs-plugins/archive/refs/tags/sdkjs-plugins.tar.gz | tar xz
+    if [[ ! -n "sdkjs-plugins-sdkjs-plugins" ]]; then
+    echo "下载汉化文件失败，请检查网络" && exit 1
+    fi
+mv sdkjs-plugins-sdkjs-plugins sdkjs-plugins
+echo -e "输入汉化文件安装目录：${red} \n（docker版直接回车，默认目录$dir）\n"
+read -p "直接回车，apt等方式安装的自己查找目录输入" webdir
+echo -e "${red}"
+    if [[ ! -n "$webdir" ]]; then
+        webdir=$dir
+    fi
+cp -rf  sdkjs-plugins $webdir
+
+echo -e "${plain}"
+    if [[ ! -n "$webdir/sdkjs-plugins/README.md" ]]; then
+        echo "${red}汉化成功，清空浏览器缓存即可看到onlyoffice插件是中文到了"
+    else
+        echo "痛哭，汉化失败了${plain}"
+    fi
